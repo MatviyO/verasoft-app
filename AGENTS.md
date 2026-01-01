e7a6c3582c1c408708e46cab0222cd8aa18d2993
Matvii Oleh <oleh.m97@gmail.com>
2026-01-01 21:20:20 +0200

    feat: init vite react project

---
+++ AGENTS.md
@@ -0,0 +1,28 @@
+## Architecture
+
+- Feature-based modular structure
+- Redux Toolkit for state management
+- Redux-Saga for side effects
+- Axios isolated in API layer
+- Dumb UI components
+- Fully implemented async flows with error handling
+
+## Feature-based (FSD-style) + Redux-Saga
+
+✔ Decoupling
+✔ Modularity
+✔ Redux / Saga
+✔ TypeScript
+✔ Axios
+✔ Enterprise-ready
+
+MUST follow best SOLID, DRY, KISS, YAGNI, GRASP
+
+
+## REQUIREMENTS
+- TypeScript 
+- React / Redux architecture 
+- Use A xios library for API calls 
+- Use redux - saga library for side effects 
+- Full completeness of the implementation 
+- Decoupling and Modularity for both folder structure and components
---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...reactPlugin.configs.flat.recommended,
+      ...reactPlugin.configs.flat['jsx-runtime'],
+      reactHooks.configs.flat.recommended,
+      jsxA11y.flatConfigs.recommended,
+      reactRefresh.configs.vite,
+    ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.serviceworker,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+])
